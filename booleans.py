ca6dd697708984a1c8e97e7baedf47709620da2f
Satyam Soni <94950988+satyampsoni@users.noreply.github.com>
2022-08-21 19:43:11 +0530

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,17 @@
+#Booleans 
+'''Booleans represnt one of two values True or False'''
+
+#If something is true it will represnt true 
+print(8>5)
+print("satyam" == "satyam") 
+#If something is false it will represent false
+
+print(5>8)
+print("satyam" == "shivam")
+
+#evalutaions of values
+x = "hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
